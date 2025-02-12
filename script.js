ce53facb8f97eaae23fdd081b8c0e4110580c089
halimawbumayo <rmtcervantes17@gmail.com>
2025-02-12 17:34:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let leftPos = 0
 let bottomPos = 0
 document.getElementById('yessir').addEventListener('click', () => {
-    alert('Congratulations! You have been invited to a Valentine\'s Day dinner. Please RSVP by February 10th. We hope to see you there!')
+    alert('Thank you for saying YES, mi love! I can\'t wait to spend this Valentine\'s Day with you')
 })
 document.getElementById('nooo').addEventListener('mouseover', () => {
     leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
